f967376629c6315d23f242706301becb74aa8d9d
seyitserin1-jpg <seyitserin1@gmail.com>
2026-08-13 13:53:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitplan-v11-health-os';
+const CACHE='fitplan-v13-diet-reminderss';
 const ASSETS=['./','./index.html','./style.css?v=11.0.0','./app.js?v=11.0.0','./config.js','./manifest.json','./icon.svg','./VERSION.txt'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
